824f6324a7cabb54b592f080f6d99b6c2f1d7941
CS-parth <parth.vijy@gmail.com>
2024-08-09 23:09:12 +0530

    delete: deleted old useHistory usage

--- src/hooks/useHistoryState.ts
+++ src/hooks/useHistoryState.ts
@@ -58,36 +58,3 @@ export const useHistoryStore = create<HistoryState & Actions>()(
     }
   )
 );
-
-// export const useHistory = (initialState: ElementType[]) => {
-//   const { history, setHistory, index, undo, redo } = useHistoryStore();
-//   return [history[index] || initialState, setHistory, undo, redo] as const;
-// };
-
-
-// import { useState } from "react";
-
-// export const useHistory = (initialState) => {
-//   const [index, setIndex] = useState(0);
-//   const [history, setHistory] = useState([initialState]);
-
-//   const setState = (action, overwrite = false) => {
-//     const newState =
-//       typeof action === "function" ? action(history[index]) : action;
-//     if (overwrite) {
-//       const historyCopy = [...history];
-//       historyCopy[index] = newState;
-//       setHistory(historyCopy);
-//     } else {
-//       const updatedState = [...history].slice(0, index + 1);
-//       setHistory([...updatedState, newState]);
-//       setIndex((prevState) => prevState + 1);
-//     }
-//   };
-
-//   const undo = () => index > 0 && setIndex((prevState) => prevState - 1);
-//   const redo = () =>
-//     index < history.length - 1 && setIndex((prevState) => prevState + 1);
-
-//   return [history[index], setState, undo, redo];
-// };

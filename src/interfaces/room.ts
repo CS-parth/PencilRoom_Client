fb351c391924e849f5637b24e62abf5109af075f
CS-parth <parth.vijy@gmail.com>
2024-10-14 21:44:23 +0530

    [Feat] [Done] [Parth] : Implemented all the interfaces required to be acced by the whiteboard

---
+++ src/interfaces/room.ts
@@ -0,0 +1,35 @@
+// import { GameDifficulties, GameDuration, GameModes } from './game';
+import { User } from './user';
+
+// export interface GameSettings {
+//   mode: GameModes;
+//   difficulty: GameDifficulties;
+//   duration: GameDuration;
+// }
+
+export enum RoomStatus {
+  WAITING,
+  RUNNING,
+  FINISHED,
+}
+
+// export interface Progress {
+//   wpm: number;
+//   accuracy: number;
+//   correctWordsArray: string[];
+//   incorrectWordsArray: string[];
+// }
+
+export interface Room {
+  roomId?: string;
+  owner?: User;
+  users?: User[];
+//   usersProgress?: {
+//     key: string;
+//     value: Progress;
+//   }[];
+//   gameSettings: GameSettings;
+  status?: RoomStatus;
+  createdAt?: Date;
+  timer?: number;
+}
---
+++ src/interfaces/whiteboard.ts
@@ -0,0 +1,43 @@
+  export type SelectedElementType = ElementType & {
+    xOffsets?: number[];
+    yOffsets?: number[]; // For pencil
+    offsetX?: number; // For Others 
+    offsetY?: number;
+  };
+  
+  export type ElementType = {
+    id: number,
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    type: Tools,
+    text?: String,
+    offsetX?: number, // conditional property
+    offsetY?: number,
+    position?: string | null,
+    points?: {x:number,y:number}[];
+    roughElement?: any
+  }
+  export interface ExtendedElementType extends ElementType {
+    xOffsets?: number[];
+    yOffsets?: number[];
+  }
+  
+  export enum Tools {
+    Selection = "selection",
+    Line = "line",
+    Rectangle = "rectangle",
+    Pencil = "pencil",
+    Text = "text"
+  }
+  
+  export enum Action {
+    None = "none",
+    Drawing = "drawing",
+    Moving = "moving",
+    Selecting = "selecting",
+    Resizing = "resizing",
+    Writing = "writing",
+    Panning = "panning"
+  }

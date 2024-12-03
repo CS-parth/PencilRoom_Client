5d7fb206ea1a1cb8b48c7cc95257ec5b28503981
CS-parth <parth.vijy@gmail.com>
2024-12-03 11:23:10 +0530

    [Update] [Parth] [Done] : Updated the server url

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -7,7 +7,7 @@ import {
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === 'production'
-    ? "https://pencilroom-server.onrender.com"
+    ? "https://pencilroom-server-oj5s.onrender.com"
     : 'http://localhost:6600';
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL);
